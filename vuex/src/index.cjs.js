ea86358932714e74aafb4224572b2aa3a14e7e94
Martus Gábor <gabesztx@gmail.com>
2020-09-03 21:35:21 +0200

    vuex-playground

---
+++ vuex/src/index.cjs.js
@@ -0,0 +1,21 @@
+import { Store, install } from "./store";
+import {
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers
+} from "./helpers";
+import createLogger from "./plugins/logger";
+
+export default {
+  Store,
+  install,
+  version: "__VERSION__",
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+};
---
+++ vuex/src/index.js
@@ -0,0 +1,32 @@
+import { Store, install } from "./store";
+import {
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers
+} from "./helpers";
+import createLogger from "./plugins/logger";
+
+export default {
+  Store,
+  install,
+  version: "__VERSION__",
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+};
+
+export {
+  Store,
+  install,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+};
