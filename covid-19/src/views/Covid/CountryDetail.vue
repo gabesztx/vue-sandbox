d07b27fd599eb74068c538a92d1050867626ee2f
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2020-10-15 20:39:25 +0200

    wip

--- covid-19/src/views/Covid/CountryDetail.vue
+++ covid-19/src/views/Covid/CountryDetail.vue
@@ -1,54 +1,76 @@
 <template>
   <div class="page-content countryDetail">
-    <section class="hero is-large">
-      <div class="hero-body">
-
-        <div class="container">
-
-          <div class="box">
-            <div class="box-header-content">
-              <div class="level">
-                <div class="level-left">
-                  <div class="level-item">
-                    <img class="country-flag" src="http://localhost:5000/static/flags/ci.svg" />
-                  </div>
-                  <div class="level-item">
-                    <div class="title">United States</div>
-                  </div>
-                </div>
-                <div class="level-right">
-                  <div class="subtitle">2012-02-2</div>
-                </div>
-              </div>
-            </div>
-
-
-            <div class="box-body-content">
-              <div class="tile is-ancestor">
-                <div class="tile is-parent">
-                  <div class="card">
-                    <div class="card-content">
-                      <div class="level is-mobile">
-                        <div class="level-item">
-                          <div class="is-widget-label"><h3 class="subtitle is-spaced"> Clients </h3>
-                            <h1 class="title">
-                              <div>512</div>
-                            </h1>
-                          </div>
-                        </div>
-                        <div class="level-item has-widget-icon">
-                          <div class="is-widget-icon"><span class="icon has-text-primary is-large"><i
-                            class="mdi mdi-account-multiple mdi-48px"></i></span></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
+    <section class="section">
+      <div class="container">
+        <div class="box detail-content">
+          <div class="header-content">
+            <img class="country-flag" src="http://localhost:5000/static/flags/ci.svg" />
+            <div class="title">United States</div>
           </div>
+          <div class="body-content">
+            <div class="card-item-content">
+              <div class="box">
+                <div class="">Hello World</div>
+                <div class="">What is up?</div>
+              </div>
+<!--              <div class="box">
+                <p class="">Hello World</p>
+                <p class="">What is up?</p>
+              </div>
+              <div class="box">
+                <p class="">Hello World</p>
+                <p class="">What is up?</p>
+              </div>
+              <div class="box">
+                <p class="">Hello World</p>
+                <p class="">What is up?</p>
+              </div>
+              <div class="box">
+                <p class="">Hello World</p>
+                <p class="">What is up?</p>
+              </div>
+              <div class="box">
+                <p class="">Hello World</p>
+                <p class="">What is up?</p>
+              </div>-->
 
+              <!--<div class="card">
+                <div class="card-content">
+                  <p class="">Hello World</p>
+                  <p class="">What is up?</p>
+                </div>
+              </div>-->
+            </div>
+            <!--  <div class="tile is-ancestor">
+                <div class="tile is-parent">
+                  <div class="tile">
+                    <div class="">Clients </div>
+                    <div class="">512</div>
+
+                  </div>
+                </div>
+              </div>-->
+            <!-- <div class="level">
+             <div class="level-left">
+               <div class="level-item">
+               </div>
+               <div class="level-item">
+               </div>
+             </div>
+             <div class="level-right">
+             </div>
+           </div>-->
+            <!--<div class="columns">
+              <div class="column is-2">
+                <div class="card">
+                  <div class="card-content">
+                    <h1 class="title">512</h1>
+                    <h3 class="subtitle is-spaced">Clients </h3>
+                  </div>
+                </div>
+              </div>
+            </div>-->
+          </div>
         </div>
       </div>
     </section>
