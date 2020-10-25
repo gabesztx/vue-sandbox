9fafdfae377559a76034ec9fa9c41d8e3a033dbb
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2020-10-25 03:45:07 +0100

    wip

--- covid-19/src/views/Covid/CountryDetail.vue
+++ covid-19/src/views/Covid/CountryDetail.vue
@@ -1,11 +1,77 @@
 <template>
   <div class="page-content countryDetail">
-    <section class="section">
+    <section class="header-content hero is-white">
+      <div class="hero-body">
+        <div class="container">
+          <!--<div class="title">{{ $t('country') }}</div>-->
+          <div class="country-header">
+
+            <div class="country-left">
+              <img class="country-flag" src="http://localhost:5000/static/flags/ci.svg" />
+              <div class="country-text">
+                <h1 class="title">Hungary</h1>
+                <h3 class="subtitle is-6">Last update <b>2012.13.11</b></h3>
+              </div>
+            </div>
+
+            <div class="country-right">
+              <div class="btn-content">
+                <b-button
+                  :class="'button'"
+                  :label="'Country'"
+                  :size="''"
+                  :type="'is-light'">
+                </b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section body-content">
+      <div class="container">
+        
+        <div class="columns is-mobile">
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <div class="subtitle">Clients</div>
+              </div>
+            </div>
+          </div>
+        </div>
+    <!--    <div class="columns is-mobile">
+          <div class="column">
+            <div class="columns">
+              <div class="column is-2">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="subtitle">Clients</div>
+                  </div>
+                </div>
+              </div>
+              <div class="column is-2">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="subtitle">Clients</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>-->
+      </div>
+
+
+    </section>
+
+
+    <!--<section class="section">
       <div class="container">
         <div class="box detail-content">
           <div class="header-content">
             <img class="country-flag" src="http://localhost:5000/static/flags/ci.svg" />
-            <div class="title">{{$t('country')}}</div>
+            <div class="title">{{ $t('country') }}</div>
           </div>
           <div class="body-content">
             <div class="card-item-content">
@@ -15,10 +81,6 @@
                 <div class="">What is up?</div>
               </div>
 
-              <!--<div class="box">
-                <p class="">Hello World</p>
-                <p class="">What is up?</p>
-              </div>
               <div class="box">
                 <p class="">Hello World</p>
                 <p class="">What is up?</p>
@@ -34,16 +96,20 @@
               <div class="box">
                 <p class="">Hello World</p>
                 <p class="">What is up?</p>
-              </div>-->
+              </div>
+              <div class="box">
+                <p class="">Hello World</p>
+                <p class="">What is up?</p>
+              </div>
 
-              <!--<div class="card">
+              &lt;!&ndash;<div class="card">
                 <div class="card-content">
                   <p class="">Hello World</p>
                   <p class="">What is up?</p>
                 </div>
-              </div>-->
+              </div>&ndash;&gt;
             </div>
-            <!--  <div class="tile is-ancestor">
+            &lt;!&ndash;  <div class="tile is-ancestor">
                 <div class="tile is-parent">
                   <div class="tile">
                     <div class="">Clients </div>
@@ -51,8 +117,8 @@
 
                   </div>
                 </div>
-              </div>-->
-            <!-- <div class="level">
+              </div>&ndash;&gt;
+            &lt;!&ndash; <div class="level">
              <div class="level-left">
                <div class="level-item">
                </div>
@@ -61,8 +127,8 @@
              </div>
              <div class="level-right">
              </div>
-           </div>-->
-            <!--<div class="columns">
+           </div>&ndash;&gt;
+            &lt;!&ndash;<div class="columns">
               <div class="column is-2">
                 <div class="card">
                   <div class="card-content">
@@ -71,11 +137,11 @@
                   </div>
                 </div>
               </div>
-            </div>-->
+            </div>&ndash;&gt;
           </div>
         </div>
       </div>
-    </section>
+    </section>-->
     <!--<button v-on:click="onClick">Back</button>-->
   </div>
 </template>
@@ -83,7 +149,7 @@
   import router from '@/router';
 
   export default {
-    setup() {
+    setup(){
       const onClick = () => {
         router.push({ path: '/country' });
       };
