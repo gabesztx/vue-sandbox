f43b1f99bca62c21a0ad94cd8b7fa0ec3bb2690e
Gabesz <gabor.martus@icellmobilsoft.hu>
2020-11-19 20:23:09 +0100

    vue

--- covid-19/server.ts
+++ covid-19/server.ts
@@ -1,20 +1,20 @@
 // const path = require('path');
-import path from "path";
-import http from "http";
-import cors from "cors";
-import history from "connect-history-api-fallback";
-import bodyParser from "body-parser";
-import express from "express";
+import path from 'path';
+import http from 'http';
+import cors from 'cors';
+import history from 'connect-history-api-fallback';
+import bodyParser from 'body-parser';
+import express from 'express';
 
 const port = process.env.PORT || 5000;
-const basePath = path.join(__dirname, "./build");
+const basePath = path.join(__dirname, './build');
 
 /* App */
 const app = express();
 app.use(history());
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(basePath));
 
 /* Services */
@@ -23,26 +23,26 @@ import {
   getCovid19CountryDB,
   getCovid19CountryDetail,
   transformCovidDbData,
-} from "./server/services/covid-data.service";
+} from './server/services/covid-data.service';
 
 /* Server */
 const server = http.createServer(app);
 
 // TODO: app.get routerek kiszervezése
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   return res.sendFile(`${basePath}/index.html`);
 });
 
-app.get("/continent", (req, res) => {
+app.get('/continent', (req, res) => {
   return res.send(getCovid19ContinentDB());
 });
-app.get("/world", (req, res) => {
+app.get('/world', (req, res) => {
   return res.send(getCovid19ContinentDB()[0]);
 });
-app.get("/countries", (req, res) => {
+app.get('/countries', (req, res) => {
   return res.send(getCovid19CountryDB());
 });
-app.get("/countries/:countryCode", (req, res) => {
+app.get('/countries/:countryCode', (req, res) => {
   return res.send(getCovid19CountryDetail(req.params.countryCode));
 });
 
--- covid-19/server/helpers/node-helper.ts
+++ covid-19/server/helpers/node-helper.ts
@@ -1,3 +1,4 @@
 import path from 'path';
+
 export const basePath = path.join(__dirname, '../../');
 export const port = process.env.PORT || 5000;
