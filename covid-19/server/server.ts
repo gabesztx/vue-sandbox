02bb282dfdbb6109d40b45cf4938832ffb9aaa8a
Gabesz <gabor.martus@icellmobilsoft.hu>
2021-01-05 23:49:51 +0100

    wip

--- covid-19/server/server.ts
+++ covid-19/server/server.ts
@@ -8,28 +8,23 @@ import express from 'express';
 import http from 'http';
 import https from 'https';
 
-export const basePath = path.join(__dirname, './');
 const dev = process.env.ENV === 'dev';
-// const port = process.env.PORT || dev ? 80 : 443;
-const port = 80;
-
+const port = process.env.PORT || dev ? 80 : 443;
+export const basePath = path.join(__dirname, './');
 
 const app = express();
-/*const server = !dev ?
+const server = !dev ?
   https.createServer({
     key: fs.readFileSync('/etc/letsencrypt/live/covid.duckdns.org/privkey.pem', 'utf8'),
     cert: fs.readFileSync('/etc/letsencrypt/live/covid.duckdns.org/cert.pem', 'utf8'),
     ca: fs.readFileSync('/etc/letsencrypt/live/covid.duckdns.org/chain.pem', 'utf8'),
   }, app) :
-  http.createServer(app);*/
-
-const server = http.createServer(app);
-// const httpsServer = https.createServer(option, app);
+  http.createServer(app);
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
-// app.use(history());
-// app.use(cors());
+app.use(history());
+app.use(cors());
 app.use(express.static(`${basePath}`, {dotfiles: 'allow'}));
 
 // TODO: app.get routerek kiszervezése: Then, load the router module in the app:
@@ -53,11 +48,4 @@ server.listen(port, () => {
 });
 
 // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/kbsz.duckdns.org/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/kbsz.duckdns.org/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/kbsz.duckdns.org/chain.pem', 'utf8');
-// const option = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
+// const httpsServer = https.createServer(option, app);
