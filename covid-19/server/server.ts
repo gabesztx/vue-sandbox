8e0dd039df445536907d5edf15cf05b71a3970e9
Gabesz <gabor.martus@icellmobilsoft.hu>
2021-01-05 19:08:26 +0100

    testing

--- covid-19/server/server.ts
+++ covid-19/server/server.ts
@@ -1,12 +1,13 @@
 // import cors from 'cors';
-const cors = require('cors')
+const cors = require('cors');
 import history from 'connect-history-api-fallback';
 import bodyParser from 'body-parser';
 import express from 'express';
 import http from 'http';
 import path from 'path';
+
 export const basePath = path.join(__dirname, './');
-export const port = process.env.PORT || 5000;
+export const port = process.env.PORT || 80;
 import {
   // getCovid19World,
   // getCovid19Continents,
@@ -14,7 +15,6 @@ import {
   // getCovid19CountryDetail,
   transformCovidDbData,
 } from './services/covid-data.service';
-
 
 
 /* App */
@@ -25,7 +25,7 @@ app.use(history());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-app.use(express.static(`${basePath}`));
+app.use(express.static(`${basePath}`, {dotfiles: 'allow'}));
 
 // TODO: app.get routerek kiszervezése: Then, load the router module in the app:
 // https://expressjs.com/en/guide/routing.html
@@ -48,6 +48,7 @@ app.get('/getContinents', (req, res) => {
   // res.send(getCovid19Continents());
 });
 server.listen(port, () => {
-  transformCovidDbData();
+  console.log('Server started!', 'Port:', port);
+  // transformCovidDbData();
 });
 // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
