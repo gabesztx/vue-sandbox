c0e01de208b748d893a72b7d4dc7853b98bbf787
Gabesz <gabor.martus@icellmobilsoft.hu>
2021-03-09 02:35:57 +0100

    we

--- covid-19/server/services/covid-data.service.ts
+++ covid-19/server/services/covid-data.service.ts
@@ -133,14 +133,14 @@ const getCountryList = (countryNames: any[], dayNumber?: number) => {
 };
 
 export const transformCovidDbData = async () => {
-  const countryNames = await jsFileService.readJsonFile(`${basePath}/db/countries.json`);
-  const allData = await Promise.all([
-    getContinentList(),
-    getCountryList(countryNames, 0),
-    getCountryList(countryNames, 1),
-    getCountryList(countryNames, 2),
-  ]);
-  const continents = await getContinentList();
+  // const countryNames = await jsFileService.readJsonFile(`${basePath}/db/countries.json`);
+  // const allData = await Promise.all([
+  //   getContinentList(),
+  //   getCountryList(countryNames, 0),
+  //   getCountryList(countryNames, 1),
+  //   getCountryList(countryNames, 2),
+  // ]);
+  // const continents = await getContinentList();
   console.log('All Done!');
 
   // TODO: adatok betöltése client oldalon :)
