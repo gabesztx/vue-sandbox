62b021ef76340f87517b2e36a982e6d79a572a36
gabesz <gabesztx@gmail.com>
2021-07-09 09:36:53 +0200

    last commit - 1

--- covid-19/src/main.ts
+++ covid-19/src/main.ts
@@ -44,3 +44,4 @@ new Vue({
 // Vue.component('v-page-country', CountryComponent);
 // Vue.component('v-page-country-detail', CountryDetailComponent);
 // Vue.component('v-page', PageComponent);
+console.log('-----------dskndksndksndjsndsjndjndsjn');
