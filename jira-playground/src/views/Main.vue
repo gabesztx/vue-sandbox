a98157039e1ba98aabb9c2b72433434bee3e8ac5
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2021-12-09 19:44:00 +0100

    init

---
+++ jira-playground/src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">Hello</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Main",
+  // components: {
+  //   HelloWorld,
+  // },
+});
+</script>
