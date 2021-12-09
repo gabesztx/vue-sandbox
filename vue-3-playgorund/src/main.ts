32e3a93934962a398547b04756c209caccf9c115
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2021-12-09 19:28:17 +0100

    delete

--- vue-3-playgorund/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import vuetify from "./plugins/vuetify";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import i18n from "./i18n";
-
-createApp(App).use(i18n).use(router).use(store).use(vuetify).mount("#app");
