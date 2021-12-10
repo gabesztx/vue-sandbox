e20d4e08332e5e47cb620a0c5312034ff1f521e3
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2021-12-10 02:28:38 +0100

    new

---
+++ jira-data/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  setup: () => {
+    console.log("setup");
+    return {};
+  },
+});
+</script>
