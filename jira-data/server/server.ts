bace1925279e8a1bae35220622bfa3a37e69a43c
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2021-12-12 21:44:51 +0100

    webhook

--- jira-data/server/server.ts
+++ jira-data/server/server.ts
@@ -11,8 +11,8 @@ import https from 'https';
 const app = express();
 const basePath = path.join(__dirname, './');
 const dev = process.env.ENV === 'dev';
-
 const port = process.env.PORT || dev ? 80 : 443;
+
 const server = !dev ? https.createServer(
     {
       key: fs.readFileSync('/etc/letsencrypt/live/gabesztx.duckdns.org/privkey.pem', 'utf8'),
@@ -21,27 +21,25 @@ const server = !dev ? https.createServer(
     }, app)
   : http.createServer(app);
 
-
-// const port = 80;
-// const server = http.createServer(app);
-// app.use(express.static(`${basePath}`));
-
 app.use(history());
 app.use(cors());
 app.use(express.static(`${basePath}`, {dotfiles: 'allow'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// const port = 80;
+// const server = http.createServer(app);
+// app.use(express.static(`${basePath}`));
 
 const dummyResponse = {
   name: 'dummy'
 };
 // https://expressjs.com/en/guide/routing.html
-app.get('/getData', (req, res) => {
+/*app.get('/getData', (req, res) => {
   res.send(dummyResponse);
-});
-app.post('/postData', (req, res) => {
-  console.log('POST:', req.body);
+});*/
+app.post('/webhook', (req, res) => {
+  console.log('webhook:', req.body);
   res.send(dummyResponse);
 });
 server.listen(port, () => {
