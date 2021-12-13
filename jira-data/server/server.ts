2e552b87aa5059943d1751214598470dcf931e57
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2021-12-13 01:23:13 +0100

    changes

--- jira-data/server/server.ts
+++ jira-data/server/server.ts
@@ -1,17 +1,16 @@
-import bodyParser from 'body-parser';
-// const cors = require('cors');
-import cors from 'cors';
-import express from 'express';
+// import path from 'path';
 import fs from 'fs';
-import path from 'path';
-import history from 'connect-history-api-fallback';
 import http from 'http';
 import https from 'https';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import history from 'connect-history-api-fallback';
 
 const app = express();
-const dev = process.env.ENV !== 'dev';
-const port = process.env.PORT || dev ? 80 : 443;
-const server = dev ?
+const dev = process.env.ENV === 'dev';
+const port = process.env.PORT || !dev ? 80 : 443;
+const server = !dev ?
   http.createServer(app) :
   https.createServer(
     {
@@ -19,8 +18,6 @@ const server = dev ?
       cert: fs.readFileSync('/etc/letsencrypt/live/gabesztx.duckdns.org/cert.pem', 'utf8'),
       ca: fs.readFileSync('/etc/letsencrypt/live/gabesztx.duckdns.org/chain.pem', 'utf8'),
     }, app);
-
-// const basePath = path.join(__dirname, './');
 
 app.use(history());
 app.use(cors());
@@ -36,5 +33,6 @@ server.listen(port, () => {
   console.log('Server is running!', 'Port:', port);
 });
 
+// const basePath = path.join(__dirname, './');
 // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
 // const httpsServer = https.createServer(option, app);
