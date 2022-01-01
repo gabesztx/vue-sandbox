790b759bbd3ddeaba236b87a79b04354ff33c3c5
Gabesz <gabor.martus@icellmobilsoft.hu>
2022-01-01 21:31:29 +0100

    up

--- jira-data/src/core/api/jira.board.api.ts
+++ jira-data/src/core/api/jira.board.api.ts
@@ -1,41 +1,26 @@
 import http from "@/core/services/http.service";
 import { Board, BoardListQuery } from "@/core/dataTypes/types/board.interface";
-import { EpicsQuery } from "@/core/dataTypes/types/epics.interface";
-import { ProjectQuery } from "@/core/dataTypes/types/project.interface";
 
 export const getAllBoards = (): Promise<BoardListQuery> =>
-  http
-  .get<never>(`/rest/agile/1.0/board`)
+  http.get<never>(`/rest/agile/1.0/board`)
   // .get<never>(`/rest/api/3/dashboard`)
   .then((response) => response.data);
 
-export const getBoard = (boardId: number): Promise<Board> =>
-  http.get<never>(`/rest/agile/1.0/board/${boardId}`)
-  .then((response) => response.data);
-
-export const getBoardEpics = (boardId: number): Promise<EpicsQuery> =>
-  http.get<never>(`/rest/agile/1.0/board/${boardId}/epic?startAt=0&maxResults=50`)
-  .then((response) => response.data);
-
-export const getBoardProjects = (boardId: number): Promise<ProjectQuery> =>
-  http.get<never>(`/rest/agile/1.0/board/${boardId}/project`)
-  .then((response) => response.data);
-
-export const getBoardIssue = (boardId: number): Promise<any> =>
-  // jql=issuetype=Story%20AND%20status="In%20Progress"%20AND%20assignee=gabor.martus
-  http.get<never>(`/rest/agile/1.0/board/${boardId}/issue?startAt=0&maxResults=50`)
-  .then((response) => response.data);
-
-export const getBoardAllSprint = (boardId:number): Promise<any> =>
+export const getSprintsFromBoard = (boardId: number): Promise<any> =>
   http.get<never>(`/rest/agile/1.0/board/${boardId}/sprint`)
   .then((response) => response.data);
 
-
-export const getBoardAllQuickFilters = (sprintId: number): Promise<any> =>
+export const getIssuesFromSprint = (sprintId: number): Promise<any> =>
   // http.get<never>(`/rest/agile/latest/sprint/${sprintId}/issue?jql=issuetype=Story`)
-  http.get<never>(`/rest/agile/1.0/sprint/${sprintId}/issue?jql=issuetype=Story`)
+  // http.get<never>(`/rest/agile/1.0/sprint/${sprintId}/issue?jql=issuetype=Story`)
+  http.get<never>(`/rest/api/2/issue/138044`)
   .then((response) => response.data);
 
+
+/*export const getBoardIssue = (boardId: number): Promise<any> =>
+  // jql=issuetype=Story%20AND%20status="In%20Progress"%20AND%20assignee=gabor.martus
+  http.get<never>(`/rest/agile/1.0/board/${boardId}/issue?startAt=0&maxResults=50`)
+  .then((response) => response.data);*/
 
 // V3_API
 /*const bodyData = {
--- jira-data/src/views/Home.vue
+++ jira-data/src/views/Home.vue
@@ -2,26 +2,10 @@
   <div class="home">
     <h1>Home</h1>
     <div>
-      <button @click="onClickGetAllBoards">Get All Boards</button>
-    </div>
-
-    <div>
-      <button @click="onClickGetBoard">Get Hugo Board</button>
+      <button @click="onClickGetSprint">Get Sprints from Board</button>
     </div>
     <div>
-      <button @click="onClickGetEpicFromBoard">Get Epic from Board</button>
-    </div>
-    <div>
-      <button @click="onClickGetProject">Get Project from Board</button>
-    </div>
-    <div>
-      <button @click="onClickGetIssue">Get Issue from Board</button>
-    </div>
-    <div>
-      <button @click="onClickGetSprint">Get Sprint from Board</button>
-    </div>
-    <div>
-      <button @click="onClickGetStuff">Get Stuff from Board</button>
+      <button @click="onClickGetIssues">Get Issues from Active Sprint</button>
     </div>
 
   </div>
@@ -30,66 +14,29 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
-  getAllBoards,
-  getBoard,
-  getBoardEpics,
-  getBoardProjects,
-  getBoardIssue,
-  getBoardAllQuickFilters, getBoardAllSprint
+  getIssuesFromSprint,
+  getSprintsFromBoard
 } from "@/core/api/jira.board.api";
 
 export default defineComponent({
   name: "Home",
   setup: () => {
-    const board = 223;
-    const onClickGetAllBoards = () => {
-      getAllBoards().then((res) => {
-        console.log(res);
-      });
-
-    };
-    const onClickGetBoard = () => {
-      getBoard(board).then((res) => {
-        console.log(res);
-      });
-    };
-    const onClickGetEpicFromBoard = () => {
-      getBoardEpics(board).then((res) => {
-        console.log(res);
-      });
-    };
-    const onClickGetProject = () => {
-      getBoardProjects(board).then((res) => {
-        console.log(res);
-      });
-    };
-    const onClickGetIssue = () => {
-      getBoardIssue(board).then((res) => {
-        console.log(res);
-      });
-    };
+    const boardId = 223;
     const onClickGetSprint = () => {
-      getBoardAllSprint(board).then((res) => {
+      getSprintsFromBoard(boardId).then((res) => {
+        console.log(res);
+      });
+    };
+    const activeSprintId = 953
+    const onClickGetIssues = () => {
+      getIssuesFromSprint(activeSprintId).then((res) => {
         console.log(res);
       });
     };
 
-    const onClickGetStuff = () => {
-      getBoardAllQuickFilters(953).then((res) => {
-        console.log(res.issues);
-      });
-    };
-    // getBoardAllQuickFilters(950).then((res) => {
-    //   console.log(res.issues);
-    // });
     return {
-      onClickGetAllBoards,
-      onClickGetBoard,
-      onClickGetEpicFromBoard,
-      onClickGetProject,
-      onClickGetIssue,
       onClickGetSprint,
-      onClickGetStuff,
+      onClickGetIssues,
     };
   }
 });
